3ca41e8d3573390a2c6178f803aec840649fabef
Daniil Zameshaev <daniil@MacBook-Pro-Daniil.local>
2022-05-12 12:03:17 +0400

    Add controlers for creating new article and add it in DB

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,13 @@
-import {Controller, Get, Param, ParseIntPipe, Render} from '@nestjs/common';
+import {
+  Controller,
+  Render, Redirect,
+  Get, Post,
+  Param, Body,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { articles } from './articles';
+import {Article} from "./article.model";
 
 @Controller()
 export class AppController {
@@ -12,9 +19,23 @@ export class AppController {
     return { articles };
   }
 
-  @Get(':id')
+  @Get('articles/:id')
   @Render('article')
   getById(@Param('id', ParseIntPipe) id: number) {
     return articles.find(article => article.id === id);
+  }
+
+  @Get('create')
+  @Render('create-article')
+  getForm(): void {
+    return;
+  }
+
+  @Post('articles')
+  @Redirect('/', 301)
+  create(@Body() body: any): void {
+    const id = articles.length + 1;
+    const newArticle = new Article(body.title, body.content, id);
+    articles.push(newArticle);
   }
 }
