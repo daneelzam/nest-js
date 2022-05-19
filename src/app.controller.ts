fcb85bf16e63ca151386f33b56fa0ff7fc1fc62c
anamezenceva <anya.mezentseva59@yandex.ru>
2022-05-19 20:46:47 +0300

    создание пдф страницы по ссылке из запроса

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,10 @@
-import {
-  Controller,
-  Render, Redirect,
-  Get, Post,
-  Param, Body,
-  ParseIntPipe,
-} from '@nestjs/common';
-import { AppService } from './app.service';
-import { Article } from "./article.model";
+import {Body, Controller, Get, Param, ParseIntPipe, Post, Redirect, Render,} from '@nestjs/common';
+import {AppService} from './app.service';
+import {Article} from "./article.model";
+
+// почему-то не работают через импорт
+const puppeteer = require('puppeteer');
+const ncrypt = require("ncrypt-js");
 
 @Controller()
 export class AppController {
@@ -20,11 +18,31 @@ export class AppController {
     };
   }
 
+  @Post()
+   func(@Body() body: any): string{
+    const ncryptObject = new ncrypt('secretKey');
+    const url = body.url
+    const name = ncryptObject.encrypt(url);
+
+    //сохранение статьи в виде pdf
+    (async () => {
+      const browser = await puppeteer.launch()
+      const page = await browser.newPage()
+      await page.goto(url, {
+        waitUntil: 'networkidle2',
+      });
+
+      //сохранение данных в формате buffer в переменной test
+      const test = await page.pdf({ path: `pdf/${name}.pdf`, format: 'a4' })
+      await browser.close()
+    })();
+    return 'скоро мы научимся возвращать статику'
+  }
+
   @Get('articles/:id')
   @Render('article')
   async getById(@Param('id', ParseIntPipe) id: number) {
-    const article = await Article.findOne({ id });
-    return article;
+    return await Article.findOne({id});
   }
 
   @Get('create')
