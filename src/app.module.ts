d507b896e2747c74696805faab3061816b397c9f
Daniil Zameshaev <daniil@MacBook-Pro-Daniil.local>
2022-05-21 00:01:03 +0400

    add try DB connection v2.0

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,17 +7,21 @@ import { Article } from "./article.model";
 @Module({
   imports: [
     TypeOrmModule.forRoot({
+      host: process.env.HOST,
+      database: process.env.DATABASE,
+      username: process.env.USER,
+      password: process.env.PASS,
+      url: process.env.DATABASE_URL,
       type: 'postgres',
-      host: 'ec2-52-212-228-71.eu-west-1.compute.amazonaws.com',
-      database: 'dfd57jlsrkpg0i',
-      username: 'ecygbbqwxdevfe',
       port: 5432,
-      password: 'f42dd03e85136ef900cc4a462da9610c7ccd0a06209156927730a8cd9ed2b3fb',
       logging: true,
       entities: [Article],
       synchronize: true,
-      cli: 'heroku pg:psql postgresql-adjacent-18796 --app trynestjs',
-      url: 'postgres://ecygbbqwxdevfe:f42dd03e85136ef900cc4a462da9610c7ccd0a06209156927730a8cd9ed2b3fb@ec2-52-212-228-71.eu-west-1.compute.amazonaws.com:5432/dfd57jlsrkpg0i',
+      extra: {
+        ssl: {
+          rejectUnauthorized: false
+        }
+      }
     })
   ],
   controllers: [AppController],
