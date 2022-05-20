63bbb6163282803aa2c0c0d09b0a024ff12154e3
Daniil Zameshaev <daniil@MacBook-Pro-Daniil.local>
2022-05-20 22:38:39 +0400

    make it better

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,14 +8,15 @@ import { Article } from "./article.model";
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: 'ec2-52-212-228-71.eu-west-1.compute.amazonaws.com',
+      database: 'dfd57jlsrkpg0i',
+      username: 'ecygbbqwxdevfe',
       port: 5432,
-      username: 'test',
-      password: 'test',
-      database: 'test',
+      password: 'f42dd03e85136ef900cc4a462da9610c7ccd0a06209156927730a8cd9ed2b3fb',
       logging: true,
       entities: [Article],
       synchronize: true,
+      url: 'postgres://ecygbbqwxdevfe:f42dd03e85136ef900cc4a462da9610c7ccd0a06209156927730a8cd9ed2b3fb@ec2-52-212-228-71.eu-west-1.compute.amazonaws.com:5432/dfd57jlsrkpg0i',
     })
   ],
   controllers: [AppController],
