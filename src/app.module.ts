34e0a9bce9693640eb3c2d952e320502d51115b4
Daniil Zameshaev <daniil@MacBook-Pro-Daniil.local>
2022-05-20 23:42:29 +0400

    add try DB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,6 +16,7 @@ import { Article } from "./article.model";
       logging: true,
       entities: [Article],
       synchronize: true,
+      cli: 'heroku pg:psql postgresql-adjacent-18796 --app trynestjs',
       url: 'postgres://ecygbbqwxdevfe:f42dd03e85136ef900cc4a462da9610c7ccd0a06209156927730a8cd9ed2b3fb@ec2-52-212-228-71.eu-west-1.compute.amazonaws.com:5432/dfd57jlsrkpg0i',
     })
   ],
